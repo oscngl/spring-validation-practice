616f2ba07742c6c1215edd647e5f827ec6124abd
Ömer Said ÇENGEL <oscngl@gmail.com>
2022-02-08 21:59:39 +0300

    Entities created.

---
+++ src/main/java/com/osc/spring_validation_practice/entities/concretes/User.java
@@ -0,0 +1,39 @@
+package com.osc.spring_validation_practice.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "national_id")
+    private String nationalId;
+
+}
---
+++ src/main/java/com/osc/spring_validation_practice/entities/dtos/UserDto.java
@@ -0,0 +1,41 @@
+package com.osc.spring_validation_practice.entities.dtos;
+
+import lombok.Data;
+
+import javax.validation.constraints.*;
+
+@Data
+public class UserDto {
+
+    @NotEmpty(message = "Name is required!")
+    @NotBlank(message = "Name is required!")
+    private String firstName;
+
+    @NotEmpty(message = "Last Name is required!")
+    @NotBlank(message = "Last Name is required!")
+    private String lastName;
+
+    @NotEmpty(message = "Email is required!")
+    @NotBlank(message = "Email Name is required!")
+    @Email(message = "Email is not valid!")
+    private String email;
+
+    @NotEmpty(message = "Password is required!")
+    @NotBlank(message = "Password is required!")
+    @Size(min = 8, max = 20, message = "Password must contain at least 8 and maximum 20 characters!")
+    @Pattern(regexp = "^[0-9a-zA-Z]*$", message = "Password is not valid!")
+    private String password;
+
+    @NotEmpty(message = "Phone Number is required!")
+    @NotBlank(message = "Phone Number is required!")
+    @Size(min = 10, max = 10, message = "Phone Number must be 10 characters!")
+    @Pattern(regexp = "^[0-9]*$", message = "Phone Number is not valid!")
+    private String phoneNumber;
+
+    @NotEmpty(message = "National Id is required!")
+    @NotBlank(message = "National Id is required!")
+    @Size(min = 11, max = 11, message = "National Id must be 11 characters!")
+    @Pattern(regexp = "^[0-9]*$", message = "National Id is not valid!")
+    private String nationalId;
+
+}
